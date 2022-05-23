b544c743b9a6f58ff9c50f358a735c39c64af9ba
shanto <santo15-10959@diu.edu.bd>
2022-05-23 13:12:04 +0600

    add my portfolio

--- src/Pages/MyPortfolio/MyPortfolio.js
+++ src/Pages/MyPortfolio/MyPortfolio.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import mypic from '../../image/portfolio/myImage.jpg';
 
 const MyPortfolio = () => {
     return (
-        <div>
-            <h2>this is portfolio</h2>
+        <div className='bg-gray-200'>
+            <h2 className='text-center font-bold text-4xl p-5 text-pink-700'>My Portfolio</h2>
+
+            <div>
+                <div className="hero min-h-screen bg-base-200">
+                    <div className="hero-content flex-col lg:flex-row ">
+                        <img src={mypic} className="max-w-sm rounded-lg shadow-2xl" />
+                        <div className='ml-20'>
+                            <h1 className="text-4xl font-bold my-4">Name: Santo Saha
+                            </h1>
+                            <h1 className="text-xl font-bold">Email: shantosaha508@gmail.com
+                            </h1>
+                            <h1 className="text-xl font-bold my-3">Education: B.Sc in CSE at Daffodil International University
+                            </h1>
+                            <h1 className="text-xl font-bold">Tools: HTML, CSS, JavaScript, React, Node.js, MongoDB, 
+                            </h1>
+                            <p className='my-3'>
+                                Projects: <br /> 1. <a href="">Dress WareHouse</a><br /> 
+                                2. <a href="">Banking System</a> <br />
+                                <a href="">3.</a>
+                            </p>
+                         
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
     return (
         <div className="navbar bg-pink-800 text-white">
             <div className="navbar-start ">
-                <div className="dropdown">
+                <div className="dropdown ">
                     <label tabindex="0" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
